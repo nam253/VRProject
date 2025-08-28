1a793b8ccec44948d27c57e9e53a459d58270c5d
nam253 <namye6950@gmail.com>
2025-08-28 16:21:38 +0900

    fix: 편지 스크립트 수정

--- Assets/Script/Letter.cs
+++ Assets/Script/Letter.cs
@@ -4,33 +4,31 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Letter : MonoBehaviour
 {
-    private XRGrabInteractable interactable;
-    public GameObject LatterCanvas;
+    private XRSimpleInteractable interactable;
+    public GameObject LetterCanvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        interactable = GetComponent<XRGrabInteractable>();
+        interactable = GetComponent<XRSimpleInteractable>();
 
-        if(LatterCanvas != null)
+        if (LetterCanvas != null)
         {
-            LatterCanvas.SetActive(false);
+            LetterCanvas.SetActive(false);
         }
 
-    }
-
-    public void SelectEvent()
-    {
         if (interactable != null)
         {
             interactable.selectEntered.AddListener(OnSelectEntereds);
         }
+
     }
+
 
     public void OnSelectEntereds(SelectEnterEventArgs args)
     {
-        if(LatterCanvas != null)
+        if(LetterCanvas != null)
         {
-            LatterCanvas.SetActive(true);
+            LetterCanvas.SetActive(true);
         }
     }
 

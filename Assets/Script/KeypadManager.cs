a2c32a6b3b760d91c8fb1f748ff63515d4188a85
nam253 <namye6950@gmail.com>
2025-09-02 20:43:39 +0900

    Revert "feat: 카운트 다운 추가"
    This reverts commit c39579c6ece67051fa974003b8e7093689444a50.

--- Assets/Script/KeypadManager.cs
+++ Assets/Script/KeypadManager.cs
@@ -8,45 +8,22 @@ public class KeypadManager : MonoBehaviour
     public TMP_Text passwordScreen;
     public string correctCode = "1234";
     public Animator doorAnimator;
-   // public float doorStayOpenTime = 10.0f;
+    public float doorStayOpenTime = 10.0f;
 
     private string currentInput = "";
     private bool IsOpen = false;
 
     public float countdownTime = 60f;
-    public GameObject successCanvas;
-    public GameObject gameOverCanvas;
-
-    public TMP_Text countdownText;
-
+    public string successSceneName = "SuccessScene";
+    public string gameOverSceneName = "GameOverScene";
 
     private bool isGameOver = false;
 
     void Start()
     {
-        
+        // 게임 시작과 동시에 카운트다운을 시작합니다.
+        // countdownTime 후에 'GameOver' 메서드를 호출합니다.
         Invoke("GameOver", countdownTime);
-
-        if (successCanvas != null) successCanvas.SetActive(false);
-        if (gameOverCanvas != null) gameOverCanvas.SetActive(false);
-    }
-
-    void Update()
-    {
-        // 게임 오버가 아닐 때만 시간을 업데이트합니다.
-        if (!isGameOver)
-        {
-            countdownTime -= Time.deltaTime;
-            // 남은 시간을 정수로 변환하여 텍스트에 표시합니다.
-            countdownText.text = Mathf.Round(countdownTime).ToString();
-
-            // 시간이 0보다 작아지면 GameOver()를 호출합니다.
-            // Invoke를 사용하고 있지만 이중 체크용으로 추가하면 좋습니다.
-            if (countdownTime <= 0)
-            {
-                GameOver();
-            }
-        }
     }
 
 
@@ -87,43 +64,31 @@ public class KeypadManager : MonoBehaviour
 
     public void OpenDoor()
     {
-        if(isGameOver)
-        {
-            return;
-        }
-
         if(!IsOpen)
         {
             CancelInvoke("GameOver");
             doorAnimator.SetBool("IsOpen", true);
             IsOpen = true;
-
-            if(successCanvas != null)
-            {
-                successCanvas.SetActive(true);
-            }
-            //Invoke("CloseDoor", doorStayOpenTime);
+            Invoke("CloseDoor", doorStayOpenTime);
         }
     }
 
-    /*public void CloseDoor()
+    public void CloseDoor()
     {
         if(IsOpen)
         {
             doorAnimator.SetBool("IsOpen", false);
             IsOpen = false;
         }
-    }*/
+    }
 
     private void GameOver()
     {
         isGameOver = true;
         Debug.Log("게임 오버! 시간 초과!");
 
-       if (gameOverCanvas != null)
-        {
-            gameOverCanvas.SetActive(true);
-        }
+        // 게임 오버 씬으로 전환
+        SceneManager.LoadScene(gameOverSceneName);
     }
 
 }

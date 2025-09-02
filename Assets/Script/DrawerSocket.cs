1bb16741db06f8e8dd21d83b1a3b11b08e0a6c11
nam253 <namye6950@gmail.com>
2025-09-02 16:43:08 +0900

    feat: 첫번째 상호작용과 힌트

---
+++ Assets/Script/DrawerSocket.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class DrawerSocket : MonoBehaviour
+{
+    public Animator drawerAnimator;
+    public string targetObjectName = "Wine";
+    public string animatorParameterName = "IsOpenDrawer";
+
+    private bool isObjectInSocket = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == targetObjectName || other.CompareTag(targetObjectName))
+        {
+            if(!isObjectInSocket)
+            {
+                Debug.Log(targetObjectName + "이(가) 소켓에 삽입되었습니다!");
+                isObjectInSocket = true;
+
+                if(drawerAnimator != null)
+                {
+                    drawerAnimator.SetBool(animatorParameterName, true);
+                }
+            }
+        }
+    }
+
+   
+}

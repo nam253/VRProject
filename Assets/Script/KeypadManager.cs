d6a8279246adfa5012884177b2874674cf086934
nam253 <namye6950@gmail.com>
2025-09-02 20:24:24 +0900

    feat: 게임클리어 씬, 게임 클리어 씬

--- Assets/Script/KeypadManager.cs
+++ Assets/Script/KeypadManager.cs
@@ -1,6 +1,7 @@
 using SojaExiles;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KeypadManager : MonoBehaviour
 {
@@ -11,7 +12,21 @@ public class KeypadManager : MonoBehaviour
 
     private string currentInput = "";
     private bool IsOpen = false;
-    
+
+    public float countdownTime = 60f;
+    public string successSceneName = "SuccessScene";
+    public string gameOverSceneName = "GameOverScene";
+
+    private bool isGameOver = false;
+
+    void Start()
+    {
+        // 게임 시작과 동시에 카운트다운을 시작합니다.
+        // countdownTime 후에 'GameOver' 메서드를 호출합니다.
+        Invoke("GameOver", countdownTime);
+    }
+
+
     public void RefreshScreen()
     {
         passwordScreen.text = currentInput;
@@ -51,6 +66,7 @@ public class KeypadManager : MonoBehaviour
     {
         if(!IsOpen)
         {
+            CancelInvoke("GameOver");
             doorAnimator.SetBool("IsOpen", true);
             IsOpen = true;
             Invoke("CloseDoor", doorStayOpenTime);
@@ -64,6 +80,15 @@ public class KeypadManager : MonoBehaviour
             doorAnimator.SetBool("IsOpen", false);
             IsOpen = false;
         }
+    }
+
+    private void GameOver()
+    {
+        isGameOver = true;
+        Debug.Log("게임 오버! 시간 초과!");
+
+        // 게임 오버 씬으로 전환
+        SceneManager.LoadScene(gameOverSceneName);
     }
 
 }

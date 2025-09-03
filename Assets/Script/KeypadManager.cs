8d3061940775ad6a1f1ede7aca98108c4571d10f
nam253 <namye6950@gmail.com>
2025-09-03 10:45:34 +0900

    fix: 스크립트 수정, 불필요한 오브젝트, 애니메이션 연결 끊음

--- Assets/Script/KeypadManager.cs
+++ Assets/Script/KeypadManager.cs
@@ -13,20 +13,6 @@ public class KeypadManager : MonoBehaviour
     private string currentInput = "";
     private bool IsOpen = false;
 
-    public float countdownTime = 60f;
-    public string successSceneName = "SuccessScene";
-    public string gameOverSceneName = "GameOverScene";
-
-    private bool isGameOver = false;
-
-    void Start()
-    {
-        // 게임 시작과 동시에 카운트다운을 시작합니다.
-        // countdownTime 후에 'GameOver' 메서드를 호출합니다.
-        Invoke("GameOver", countdownTime);
-    }
-
-
     public void RefreshScreen()
     {
         passwordScreen.text = currentInput;
@@ -66,7 +52,6 @@ public class KeypadManager : MonoBehaviour
     {
         if(!IsOpen)
         {
-            CancelInvoke("GameOver");
             doorAnimator.SetBool("IsOpen", true);
             IsOpen = true;
             Invoke("CloseDoor", doorStayOpenTime);
@@ -80,15 +65,6 @@ public class KeypadManager : MonoBehaviour
             doorAnimator.SetBool("IsOpen", false);
             IsOpen = false;
         }
-    }
-
-    private void GameOver()
-    {
-        isGameOver = true;
-        Debug.Log("게임 오버! 시간 초과!");
-
-        // 게임 오버 씬으로 전환
-        SceneManager.LoadScene(gameOverSceneName);
     }
 
 }

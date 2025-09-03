fef6d5378cd7441dffcba581202ef948b7de6111
nam253 <namye6950@gmail.com>
2025-09-03 15:01:41 +0900

    feat: 텍스트 코루틴 함수 추가

---
+++ Assets/Script/TextEffect.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TextEffect : MonoBehaviour
+{
+    public TextMeshProUGUI Storytext;
+
+    public string fullText;
+
+    public float letterDelay = 1.0f;
+
+    public float sceneTransitionDelay = 1.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(ShowTextAndTransition());
+        
+    }
+
+    IEnumerator ShowTextAndTransition()
+    {
+        Storytext.text = "";
+
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            Storytext.text += fullText[i];
+            yield return new WaitForSeconds(letterDelay);
+        }
+
+        yield return new WaitForSeconds(sceneTransitionDelay);
+
+        SceneManager.LoadScene("Scene_02");
+
+    }
+}

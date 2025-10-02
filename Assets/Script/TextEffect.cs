dbca3c68ec88ba32a7ee0255baf225c74865413c
nam253 <namye6950@gmail.com>
2025-10-02 14:38:56 +0900

    feat: 인트로씬 수정

--- Assets/Script/TextEffect.cs
+++ Assets/Script/TextEffect.cs
@@ -14,28 +14,28 @@ public class TextEffect : MonoBehaviour
 
     public float sceneTransitionDelay = 1.0f;
 
-    public AudioSource audioSource;
 
-    public AudioClip textClip;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(ShowTextAndTransition());
-        
+
+            StartCoroutine(ShowTextAndTransition());
+
     }
 
     IEnumerator ShowTextAndTransition()
     {
+
         Storytext.text = "";
 
         for (int i = 0; i < fullText.Length; i++)
         {
             Storytext.text += fullText[i];
 
-            audioSource.PlayOneShot(textClip);
 
             yield return new WaitForSeconds(letterDelay);
         }
+
 
         yield return new WaitForSeconds(sceneTransitionDelay);
 
